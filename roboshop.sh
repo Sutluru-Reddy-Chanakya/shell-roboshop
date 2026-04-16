63b2ae8c208c205e266241c46692643b8716218e
chanakya <you@example.com>
2026-04-16 06:26:26 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,13 +2,15 @@
 
 SG_ID="sg-0c5c5675a6975d499"
 AMI_ID="ami-0220d79f3f480ecf5"
-
+SUBNET_ID="subnet-0503024fbd8ea122a"
 for instance in $@
 do
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
+    --subnet-id $SUBNET_ID \
+
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
@@ -30,3 +32,5 @@ if [ $instance == "frontend" ]; then
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
+
+          echo "IP Address: $IP"
